4e589e66942e6cba0ceccc02189474420123eaaa
kristibui <kristi.bui27@gmail.com>
2020-05-30 14:35:08 -0400

    Update about/home page.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import HomeComponent from "./home/HomeComponent";
-import me from './me.png';
+import me from './me.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons'
+import { faGithubSquare, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 import './App.css';
 
 const App = () => {
@@ -12,19 +15,34 @@ const App = () => {
       <div className='sidebar'>
 
         <div className='img-sidebar'>
-          <a href='/'>
             <img className='sidebar-pic' src={me} alt='sidebar-image'/>
-            <div className='sidebar-name'>kristi bui</div>
-            <div className='sidebar-titles'>engineer, thrifter, cat enthusiast</div>
-          </a>
+            <div className='sidebar-name'><a className='ind-link' href='/'>kristi bui</a></div>
+            <div className='sidebar-titles links'>
+              <a className='ind-link' href='/projects'>projects</a><span> // </span>
+              <a className='ind-link' href='/articles'>articles</a></div>
         </div>
 
         <div className='sidebar-info'>
+          <a className='icons' href="mailto:kristi.bui27@gmail.com">
+            <FontAwesomeIcon icon={faGithubSquare} size='2x'/>
+          </a>
+          <a className='icons' href="https://www.linkedin.com/in/kristi-bui/">
+            <FontAwesomeIcon icon={faLinkedin} size='2x'/>
+          </a>
+          <a className='icons' href="https://github.com/kristibui">
+            <FontAwesomeIcon icon={faEnvelopeSquare} size='2x'/>
+          </a>
 
-        </div>
+          {/* <div className='icons'>
+            <FontAwesomeIcon icon={faGithubSquare} size='2x'/>
+          </div>
+          <div className='icons'>
+            <FontAwesomeIcon icon={faLinkedin} size='2x'/>
+          </div>
+          <div className='icons'>
+            <FontAwesomeIcon icon={faEnvelopeSquare} size='2x'/>
+          </div> */}
 
-        <div className='links'>
-          <a className='ind-link' href='/about'>about</a> | <a className='ind-link' href='/projects'>projects</a> | <a className='ind-link' href='/articles'>articles</a> | <a className='ind-link' href='/contact'>contact</a>
         </div>
 
       </div>
--- src/home/HomeComponent.js
+++ src/home/HomeComponent.js
@@ -5,11 +5,26 @@ const HomeComponent = () => {
 
     return(
         <div className='content'>
-            <h1>welcome to my portfolio</h1>
-            <p>
-                Hi, welcome! My name is Kristi and I am a developer and a fourth-year computer science 
-                student at Northeastern University. 
-            </p>
+            <h1 className='headline'>hello ✌️ my name is kristi</h1>
+            <h2 className='sub-headline'>I am a data-passionate developer and student.</h2>
+
+            <div className='about-body'>
+                <p>
+    I currently work at <i>Capital One</i> as a <b>Software Engineering Intern</b>, and in the Fall of 2020, will be 
+                working as <i>Signify Health</i> as an <b>Analytic Engineering Intern</b>. I am a senior Computer Science student 
+                at Northeastern University, and am super interested about data engineering, data visualization, and
+                the interesection of technology and civics. 
+                </p>
+                <p>
+                Outside of 👩🏻‍💻, I'm especially passionate about photography, traveling, 
+                thrifting and sustainable living, teaching myself guitar, and playing with my cat!
+                </p>
+            </div>
+
+            <a href='/projects'>
+                <button className='learn-more-btn'>Learn More</button>
+            </a>
+
         </div>
     )
 }
