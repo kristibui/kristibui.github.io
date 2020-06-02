ddde5337d8c7ab29441069d57e038e0419ca6559
kristibui <kristi.bui27@gmail.com>
2020-06-01 20:43:44 -0400

    Add preliminary projects.

--- src/projects/ProjectComponent.js
+++ src/projects/ProjectComponent.js
@@ -1,5 +1,11 @@
 import React from "react";
-import "./project.css"
+import "./project.css";
+import polls from './images/neu_polls.png';
+import food from './images/food_reccs.png';
+import bosart from './images/boston_art_db.png';
+import mass from './images/mass_precinct_example.png';
+import animator from './images/exampleAnimator.png';
+import freecell from './images/freecellExample.png';
 
 const ProjectComponent = () => {
 
@@ -9,29 +15,45 @@ const ProjectComponent = () => {
 
             <div className='project-container'>
                 <div className='project-block grid-item'>
-                    neu polls
+                    <img className='proj-img' src={polls} alt='polls'/>
+                    <div class="overlay">
+                        <div class="proj-text">NEU Polls</div>
+                    </div>
                 </div>
 
                 <div className='project-block grid-item'>
-                    where should you eat
+                    <img className='proj-img' src={food} alt='food-recommendations'/>
+                    <div class="overlay">
+                        <div class="proj-text">What Should You Order?</div>
+                    </div>
                 </div>
 
                 <div className='project-block grid-item'>
-                    boston art
-                </div>
-            </div>
-
-            <div className='project-container'>
-                <div className='project-block grid-item'>
-                    MA marijuana legalization
+                    <img className='proj-img' src={bosart} alt='boston-art'/>
+                    <div class="overlay">
+                        <div class="proj-text">Boston Art Database</div>
+                    </div>
                 </div>
 
                 <div className='project-block grid-item'>
-                    easy animator
+                    <img className='proj-img' src={mass} alt='mass-precints'/>
+                    <div class="overlay">
+                        <div class="proj-text">MA Marijuana Legalization</div>
+                    </div>
                 </div>
 
                 <div className='project-block grid-item'>
-                    freecell
+                    <img className='proj-img' src={animator} alt='easy-animator'/>
+                    <div class="overlay">
+                        <div class="proj-text">Easy Animator</div>
+                    </div>
+                </div>
+
+                <div className='project-block grid-item'>
+                    <img className='proj-img' src={freecell} alt='freecell'/>
+                    <div class="overlay">
+                        <div class="proj-text">Freecell</div>
+                    </div>
                 </div>
             </div>
 
